d267641c4aa79d5496636b2de29ee89b3c3b79c7
罗礼豪 <1371274362@qq.com>
2022-10-19 11:48:50 +0800

    add  swift demo

---
+++ Demo/swift 版本/VerifyCodeDemo/VerifyCodeDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  VerifyCodeDemo
+//
+//  Created by 罗礼豪 on 2022/10/19.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Demo/swift 版本/VerifyCodeDemo/VerifyCodeDemo/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  VerifyCodeDemo
+//
+//  Created by 罗礼豪 on 2022/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let manager = NTESVerifyCodeManager.getInstance()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(buttonDidTipped), for: .touchUpInside)
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+        button.setTitle("加载验证码", for: .normal)
+        self.view.addSubview(button)
+    }
+
+    @objc func buttonDidTipped() {
+        manager?.configureVerifyCode("deecf3951a614b71b4b1502c072be1c1", timeout: 10)
+        manager?.delegate = self
+        manager?.mode = .normal
+        manager?.lang = .CN
+        manager?.alpha = 0.3
+        manager?.userInterfaceStyle = .dark
+        manager?.color = UIColor.black
+        manager?.protocol = .https
+        manager?.openFallBack = true
+        manager?.fallBackCount = 3
+        manager?.openVerifyCodeView()
+    }
+
+}
+
+extension ViewController: NTESVerifyCodeManagerDelegate {
+    func verifyCodeInitFinish() {
+        
+    }
+    
+    func verifyCodeInitFailed(_ error: [Any]?) {
+        
+    }
+    
+    func verifyCodeCloseWindow(_ close: NTESVerifyCodeClose) {
+        
+    }
+    
+    func verifyCodeValidateFinish(_ result: Bool, validate: String?, message: String!) {
+        if result {
+            print( "验证码成功：\(validate ?? "")")
+        } else {
+            print( "验证码失败：\(validate ?? "")")
+        }
+       
+    }
+}
+
+
+
